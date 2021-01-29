4046263ee08ca6b921696bebe1a75fc22986c838
Samuel <73540958+6c65726f79@users.noreply.github.com>
2021-01-29 16:18:02 +0100

    Move build scripts to "scripts"

---
+++ scripts/postbuild-electron.js
@@ -0,0 +1,24 @@
+#!/usr/bin/env node
+
+/**
+ * This script need to be run after copying web assets to electron
+ */
+
+const fs = require('fs');
+
+function updateHTML(filename) {
+  fs.readFile(`${filename}`, 'utf8', function(err, data) {
+    if (err) {
+      return console.log(err);
+    }
+
+    // Set Content-Security-Policy to "script-src 'self'"
+    let result = data.replace("script-src * 'unsafe-eval'", "script-src 'self'");
+
+    fs.writeFile(`${filename}`, result, 'utf8', function(err) {
+      if (err) return console.log(err);
+    });
+  });
+}
+
+updateHTML('./electron/app/index.html');
