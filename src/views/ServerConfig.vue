6592ccf13295cbe57ebe73abf9761c6e997ff98d
Samuel <73540958+6c65726f79@users.noreply.github.com>
2021-02-20 18:48:37 +0100

    Update server config

--- src/views/ServerConfig.vue
+++ src/views/ServerConfig.vue
@@ -1,106 +1,136 @@
 <template>
-  <ion-page>
-    <ion-header :translucent="true">
-      <ion-toolbar>
-        <ion-buttons slot="start">
-          <ion-back-button defaultHref="#" @click="modalClose()"></ion-back-button>
-        </ion-buttons>
-        <ion-title>Server configuration</ion-title>
-        <ion-buttons slot="end">
-          <ion-button @click="saveSettings()" fill="clear">
-            <ion-icon slot="icon-only" :ios="saveOutline" :md="saveSharp"></ion-icon>
-          </ion-button>
-        </ion-buttons>
-      </ion-toolbar>
-      <ion-toolbar>
-        <ion-segment ref="tabs" @ionChange="setTab($event.detail.value)" :value="selectedTab" scrollable>
-          <ion-segment-button :value="0" ref="segment-0">
-            <ion-label>Download</ion-label>
-          </ion-segment-button>
-          <ion-segment-button :value="1" ref="segment-1">
-            <ion-label>Network</ion-label>
-          </ion-segment-button>
-          <ion-segment-button :value="2" ref="segment-2">
-            <ion-label>Bandwith</ion-label>
-          </ion-segment-button>
-          <ion-segment-button :value="3" ref="segment-3">
-            <ion-label>Queue</ion-label>
-          </ion-segment-button>
-        </ion-segment>
-      </ion-toolbar>
-    </ion-header>
-    <!--<ion-content :fullscreen="true" class="ion-padding">-->
-    <ion-slides ref="slider" :options="slidesOptions" v-on:ionSlideTransitionEnd="slideChanged">
-      <ion-slide>
+  <ion-header>
+    <ion-toolbar>
+      <ion-buttons slot="start">
+        <ion-back-button defaultHref="#" @click="modalClose()"></ion-back-button>
+      </ion-buttons>
+      <ion-title>Server configuration</ion-title>
+      <ion-buttons slot="end">
+        <ion-button @click="saveSettings()" fill="clear">
+          <ion-icon slot="icon-only" :ios="saveOutline" :md="saveSharp"></ion-icon>
+        </ion-button>
+      </ion-buttons>
+    </ion-toolbar>
+    <ion-toolbar>
+      <ion-segment ref="tabs" @ionChange="setTab($event.detail.value)" :value="selectedTab" scrollable>
+        <ion-segment-button :value="0" ref="segment-0">
+          <ion-label>Download</ion-label>
+        </ion-segment-button>
+        <ion-segment-button :value="1" ref="segment-1">
+          <ion-label>Network</ion-label>
+        </ion-segment-button>
+        <ion-segment-button :value="2" ref="segment-2">
+          <ion-label>Bandwith</ion-label>
+        </ion-segment-button>
+        <ion-segment-button :value="3" ref="segment-3">
+          <ion-label>Queue</ion-label>
+        </ion-segment-button>
+      </ion-segment>
+    </ion-toolbar>
+  </ion-header>
+  
+  <ion-slides pager='false' ref="slider" :options="slidesOptions" v-on:ionSlideTransitionEnd="slideChanged">
+    <ion-slide>
 
-        <ion-content class="ion-padding" ref="content">
-          <ion-list>
-            <ion-list-header>
-              <ion-label>
-                {{ Locale.options }}
+      <ion-content class="ion-padding" ref="tab1">
+        <ion-list>
+          <ion-list-header>
+            <ion-label>
+              {{ Locale.general }}
+            </ion-label>
+          </ion-list-header>
+
+          <ion-item>
+            <ion-label position="floating">Default download directory</ion-label>
+            <ion-input v-model="config['download-dir']"></ion-input>
+          </ion-item>
+        </ion-list>
+
+        <ion-list>
+          <ion-list-header>
+            <ion-label>
+              Global limits
+            </ion-label>
+          </ion-list-header>
+
+          <ion-item>
+            <div class="left">
+              <ion-label position="floating">
+                {{ Locale.seedRatioLimit }}
               </ion-label>
-            </ion-list-header>
+              <ion-input v-model.number="config.seedRatioLimit" type="number"></ion-input>
+            </div>
+            <ion-toggle v-model="config.seedRatioLimited" slot="end" class="swiper-no-swiping"></ion-toggle>
+          </ion-item>
 
-            <ion-item>
-              <ion-label position="floating">Default download directory</ion-label>
-              <ion-input v-model="config['download-dir']"></ion-input>
-            </ion-item>
-
-            <ion-item>
-              <div class="left">
-                <ion-label position="floating">
-                  {{ Locale.seedRatioLimit }}
-                </ion-label>
-                <ion-input v-model.number="config.seedRatioLimit" type="number"></ion-input>
-              </div>
-              <ion-toggle v-model="config.seedRatioLimited" slot="end" class="swiper-no-swiping"></ion-toggle>
-            </ion-item>
-          </ion-list>
-
-          <ion-list>
-            <ion-list-header>
-              <ion-label>
-                Incomplete downloads
+          <ion-item>
+            <div class="left">
+              <ion-label position="floating">
+                {{ Locale.stopWhenInactive }}
               </ion-label>
-            </ion-list-header>
+              <ion-input v-model.number="config['idle-seeding-limit']" type="number"></ion-input>
+            </div>
+            <ion-toggle v-model="config['idle-seeding-limit-enabled']" slot="end" class="swiper-no-swiping"></ion-toggle>
+          </ion-item>
+        </ion-list>
 
-            <ion-item>
-              <ion-label>Add .part extension to incomplete files</ion-label>
-              <ion-toggle v-model="config['rename-partial-files']" slot="end" class="swiper-no-swiping"></ion-toggle>
-            </ion-item>
-            
+        <ion-list>
+          <ion-list-header>
+            <ion-label>
+              Incomplete downloads
+            </ion-label>
+          </ion-list-header>
 
-            <ion-item>
-              <ion-label>Use incomplete directory</ion-label>
-              <ion-toggle v-model="config['incomplete-dir-enabled']" slot="end" class="swiper-no-swiping"></ion-toggle>
-            </ion-item>
+          <ion-item>
+            <ion-label>Add .part extension to incomplete files</ion-label>
+            <ion-toggle v-model="config['rename-partial-files']" slot="end" class="swiper-no-swiping"></ion-toggle>
+          </ion-item>
+          
 
-            <ion-item>
-              <ion-label position="floating">Incomplete directory</ion-label>
-              <ion-input v-model="config['incomplete-dir']"></ion-input>
-            </ion-item>
-          </ion-list>
+          <ion-item>
+            <ion-label>Use incomplete directory</ion-label>
+            <ion-toggle v-model="config['incomplete-dir-enabled']" slot="end" class="swiper-no-swiping"></ion-toggle>
+          </ion-item>
 
-        </ion-content>
+          <ion-item>
+            <ion-label position="floating">Incomplete directory</ion-label>
+            <ion-input v-model="config['incomplete-dir']"></ion-input>
+          </ion-item>
+        </ion-list>
 
-      </ion-slide>
-      <ion-slide>
-        OK
-      </ion-slide>
-    </ion-slides>
-  </ion-page>
+      </ion-content>
+
+    </ion-slide>
+    <ion-slide>
+      2
+      <!--<ion-content class="ion-padding" ref="tab2">
+        2
+      </ion-content>-->
+    </ion-slide>
+    <ion-slide>
+      3
+      <!--<ion-content class="ion-padding" ref="tab3">
+        3
+      </ion-content>-->
+    </ion-slide>
+    <ion-slide>
+      4
+      <!--<ion-content class="ion-padding" ref="tab4">
+        4
+      </ion-content>-->
+    </ion-slide>
+  </ion-slides>
 </template>
 
 <script lang="ts">
 import { 
   isPlatform,
   modalController,
-  IonContent, 
+  IonContent,
   IonHeader, 
   IonTitle, 
   IonToolbar,
   IonButtons,
-  IonPage,
   IonBackButton,
   IonSegment,
   IonSegmentButton,
@@ -121,7 +151,9 @@ import {
 } from 'ionicons/icons';
 import { TransmissionRPC } from "../services/TransmissionRPC";
 import { defineComponent } from 'vue';
+import { Utils } from "../services/Utils";
 import { Locale } from "../services/Locale";
+import * as _ from 'lodash';
 
 export default defineComponent({
   name: 'Server configuration',
@@ -131,7 +163,6 @@ export default defineComponent({
     IonTitle, 
     IonToolbar,
     IonButtons,
-    IonPage,
     IonBackButton,
     IonSegment,
     IonSegmentButton,
@@ -148,8 +179,8 @@ export default defineComponent({
   },
   data() {
     return {
-      config: TransmissionRPC.sessionArguments,
-      selectedTab:0
+      config: _.clone(TransmissionRPC.sessionArguments),
+      selectedTab:0,
     }
   },
   setup() {
@@ -167,13 +198,16 @@ export default defineComponent({
   created() {
     console.log(this.config);
   },
+  async mounted() {
+    Utils.customScrollbar(this.$refs.tab1)
+
+    setTimeout(()=>{
+      // Workaround to fix SwiperJS when opening/closing modal multiple times
+      const slider = this.$refs.slider as Record<string,any>;
+      slider.$el.update()
+    },100)
+  },
   computed: {
-    appVersion: function(): string {
-      return `v${process.env.PACKAGE_VERSION||"1.0.0"}`;
-    },
-    isWebUI: function() {
-      return !isPlatform("electron") && !isPlatform("capacitor")
-    },
     slidesOptions: function(): Record<string,any> {
       return {
         centeredSlides:true,
@@ -181,7 +215,7 @@ export default defineComponent({
         resistanceRatio:isPlatform("ios") ? 0.85 : 0,
         simulateTouch:false
       }
-    },
+    }
   },
   methods: {
     modalClose () {
@@ -226,7 +260,7 @@ export default defineComponent({
 
 <style scoped>
 ion-content {
-  text-align: left;
+  text-align: initial;
 }
 
 .left {
