bcb6e415ced7e6b104a96f358296e81583239568
Samuel <73540958+6c65726f79@users.noreply.github.com>
2021-04-14 15:20:58 +0200

    Read hash from URL

--- src/services/FileHandler.ts
+++ src/services/FileHandler.ts
@@ -39,6 +39,15 @@ export const FileHandler = {
     }
     document.body.addEventListener("dragover", (e) => e.preventDefault(), false);
     document.body.addEventListener("drop",(e) => this.handleFileDrop(e), false);
+
+    // Read hash from URL
+    let hash = window.location.hash.toLowerCase().substring(1);
+    if(hash.match(/^\b[0-9a-f]{40}\b$/)){
+      hash = `magnet:?xt=urn:btih:${hash}`;
+    }
+    if(hash.match(/^magnet:\?xt=urn:btih:[0-9a-f]{40}/)){
+      this.readMagnet(hash);
+    }
   },
   async inputFile(): Promise<void> {
     if(isPlatform("capacitor") && (isPlatform("ios") || isPlatform("android"))){
