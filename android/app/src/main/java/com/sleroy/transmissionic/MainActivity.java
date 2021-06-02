e17bd0220e04b1c14f41e7edf652bdc89b09f1bf
Samuel Leroy <73540958+6c65726f79@users.noreply.github.com>
2021-06-02 18:27:45 +0200

    Re-add Android platform

--- android/app/src/main/java/com/sleroy/transmissionic/MainActivity.java
+++ android/app/src/main/java/com/sleroy/transmissionic/MainActivity.java
@@ -11,28 +11,28 @@ import com.whitestein.securestorage.SecureStoragePlugin;
 import java.util.ArrayList;
 
 public class MainActivity extends BridgeActivity {
-  @Override
-  public void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
 
-    // Initializes the Bridge
-    this.init(savedInstanceState, new ArrayList<Class<? extends Plugin>>() {{
-      // Additional plugins you've installed go here
-      // Ex: add(TotallyAwesomePlugin.class);
-      add(SecureStoragePlugin.class);
-    }});
-  }
+		// Initializes the Bridge
+		this.init(savedInstanceState, new ArrayList<Class<? extends Plugin>>() {{
+		  // Additional plugins you've installed go here
+		  // Ex: add(TotallyAwesomePlugin.class);
+		  add(SecureStoragePlugin.class);
+		}});
+	}
 
-  @Override
-  public void onStart() {
-    super.onStart();
-    // Android fix for enabling dark mode
-    int nightModeFlags = getResources().getConfiguration().uiMode & Configuration.UI_MODE_NIGHT_MASK;
-    WebSettings webSettings = this.bridge.getWebView().getSettings();
-    if (nightModeFlags == Configuration.UI_MODE_NIGHT_YES) {
-      String userAgent = webSettings.getUserAgentString();
-      userAgent = userAgent + " AndroidDarkMode";
-      webSettings.setUserAgentString(userAgent);
-    }
-  }
+	@Override
+	public void onStart() {
+		super.onStart();
+		// Android fix for enabling dark mode
+		int nightModeFlags = getResources().getConfiguration().uiMode & Configuration.UI_MODE_NIGHT_MASK;
+		WebSettings webSettings = this.bridge.getWebView().getSettings();
+		if (nightModeFlags == Configuration.UI_MODE_NIGHT_YES) {
+		  String userAgent = webSettings.getUserAgentString();
+		  userAgent = userAgent + " AndroidDarkMode";
+		  webSettings.setUserAgentString(userAgent);
+		}
+	}
 }
