525882b78e1b85ea8864d4b48f8b78596d5258f9
Samuel <73540958+6c65726f79@users.noreply.github.com>
2021-06-26 22:47:36 +0200

    Update @capacitor-community/electron

--- electron/src/index.ts
+++ electron/src/index.ts
@@ -1,90 +1,143 @@
-import { app, shell } from "electron";
-import fs from "fs";
-import { createCapacitorElectronApp } from "@capacitor-community/electron";
-import { autoUpdater } from "electron-updater";
-import windowStateKeeper from "electron-window-state";
-require('@electron/remote/main').initialize();
+import { app, BrowserWindow, Menu, MenuItem, nativeImage, Tray } from "electron";
+import { join } from "path";
+import electronIsDev from 'electron-is-dev';
+import electronServe from 'electron-serve';
+import { CapElectronEventEmitter, CapacitorSplashScreen, getCapacitorConfig, setupCapacitorElectronPlugins, setupElectronDeepLinking } from "@capacitor-community/electron";
 
-// The MainWindow object can be accessed via myCapacitorApp.getMainWindow()
-const myCapacitorApp = createCapacitorElectronApp({
-  splashScreen: {
-    useSplashScreen: false,
-  },
-  mainWindow: {
-    windowOptions: {
-      autoHideMenuBar: true,
-      width: 990,
-      height: 700,
-      titleBarStyle: "hidden",
-      frame: false,
-      webPreferences: {
-        devTools: false,
-        contextIsolation: true
-      }
-    }
-  }
-});
+// Get Config options from capacitor.config file
+const CapacitorFileConfig = getCapacitorConfig()
 
-const gotTheLock = app.requestSingleInstanceLock()
-let openFiles: Array<Buffer>;
+/////////////////////// Menus and Configs - Modify Freely //////////////////////////////////////////////
+const TrayMenuTemplate = [
+  new MenuItem({ label: "Quit App", role: "quit" })
+];
+const AppMenuBarMenuTemplate = [
+  { role: process.platform === "darwin" ? "appMenu" : "fileMenu" },
+  { role: "viewMenu" },
+];
+const DeepLinkingConfig = {customProtocol: CapacitorFileConfig.deepLinkingCustomProtocol ?? 'mycapacitorapp'};
+////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-if (!gotTheLock) {
-  app.quit()
-} else {
-  openFiles = getTorrents(process.argv);
+// -------------------------------------------------------------------------------------------------- //
 
-  app.setAsDefaultProtocolClient("magnet")
-
-  app.on('second-instance', (event, commandLine) => {
-    openFiles = getTorrents(commandLine);
-    const mainWindow = myCapacitorApp.getMainWindow();
-    // Someone tried to run a second instance, we should focus our window.
-    if (mainWindow) {
-      if (openFiles.length>0) mainWindow.webContents.send('file-open', openFiles)
-      if (mainWindow.isMinimized()) mainWindow.restore()
-      mainWindow.focus()
-    }
-  })
-
-  // This method will be called when Electron has finished
-  // initialization and is ready to create browser windows.
-  // Some Electron APIs can only be used after this event occurs.
-  app.on("ready", () => {
-    let mainWindowState = windowStateKeeper({
-      defaultWidth: 990,
-      defaultHeight: 700
+/////////////////////// Capacitor Electron Internals Modify At Own risk ////////////////////////////////
+class ElectronCapacitorApp {
+  private MainWindow: BrowserWindow | null = null;
+  private SplashScreen: CapacitorSplashScreen | null = null;
+  private TrayIcon: Tray | null = null;
+  private loadWebApp;
+  
+  constructor() {
+    this.loadWebApp = electronServe({
+      directory: join(app.getAppPath(), "app"),
+      // The scheme can be changed to whatever you'd like (ex: someapp)
+      scheme: CapacitorFileConfig.customUrlScheme ?? 'capacitor-electron',
     });
-    myCapacitorApp.init();
-    const mainWindow = myCapacitorApp.getMainWindow();
-    if(mainWindowState.x && mainWindowState.y){
-      mainWindow.setPosition(mainWindowState.x,mainWindowState.y);
-    }
-    mainWindow.setSize(mainWindowState.width,mainWindowState.height);
-    autoUpdater.checkForUpdatesAndNotify();
-    mainWindowState.manage(mainWindow);
-    if (mainWindow && openFiles.length>0) {
-      mainWindow.webContents.on('did-finish-load', function() {
-        mainWindow.webContents.send('file-open', openFiles)
+  }
+
+  private async loadMainWindow(thisRef: any) {
+    await thisRef.loadWebApp(thisRef.MainWindow);
+  }
+
+  getMainWindow() {
+    return this.MainWindow;
+  }
+
+  async init() {
+    this.MainWindow = new BrowserWindow({
+      show: false,
+      webPreferences: {
+        nodeIntegration: true,
+        contextIsolation: false,
+        // Use preload to inject the electron varriant overrides for capacitor plugins.
+        // Note: any windows you spawn that you want to include capacitor plugins must have this preload.
+        preload: join(app.getAppPath(), "node_modules", "@capacitor-community", "electron", "dist", "runtime", "electron-rt.js"),
+      },
+    });
+
+    this.MainWindow.on("closed", () => {
+      if (this.SplashScreen && this.SplashScreen.getSplashWindow() && !this.SplashScreen.getSplashWindow().isDestroyed()) {
+        this.SplashScreen.getSplashWindow().close();
+      }
+    });
+
+    if (CapacitorFileConfig.trayIconAndMenuEnabled) {
+      this.TrayIcon = new Tray(nativeImage.createFromPath(join(app.getAppPath(), 'assets', process.platform === "win32" ? "appIcon.ico" : "appIcon.png")));
+      this.TrayIcon.on("double-click", () => {
+        if (this.MainWindow) {
+          if (this.MainWindow.isVisible()) {
+            this.MainWindow.hide();
+          } else {
+            this.MainWindow.show();
+            this.MainWindow.focus();
+          }
+        }
       });
+      this.TrayIcon.on("click", () => {
+        if (this.MainWindow) {
+          if (this.MainWindow.isVisible()) {
+            this.MainWindow.hide();
+          } else {
+            this.MainWindow.show();
+            this.MainWindow.focus();
+          }
+        }
+      });
+      this.TrayIcon.setToolTip(app.getName());
+      this.TrayIcon.setContextMenu(
+        Menu.buildFromTemplate(TrayMenuTemplate)
+      );
     }
-  });
+
+    // Setup app windows menu bar
+    Menu.setApplicationMenu(
+      // @ts-ignore
+      Menu.buildFromTemplate(AppMenuBarMenuTemplate)
+    );
+
+    if (CapacitorFileConfig.splashScreenEnabled) {
+      this.SplashScreen = new CapacitorSplashScreen({
+        imageFilePath: join(app.getAppPath(), "assets", CapacitorFileConfig.splashScreenImageName ?? "splash.png"),
+        windowWidth: 400,
+        windowHeight: 400,
+      });
+      this.SplashScreen.init(this.loadMainWindow, this);
+    } else {
+      this.loadMainWindow(this);
+    }
+
+    // Link electron plugins in
+    setupCapacitorElectronPlugins()
+
+    this.MainWindow.webContents.on("dom-ready", () => {
+      if (CapacitorFileConfig.splashScreenEnabled) {
+        this.SplashScreen.getSplashWindow().hide();
+      }
+      if (!CapacitorFileConfig.hideMainWindowOnLaunch) {
+        this.MainWindow.show();
+      }
+      setTimeout(() => {
+        if (electronIsDev) {
+          this.MainWindow.webContents.openDevTools();
+        }
+        CapElectronEventEmitter.emit("CAPELECTRON_DeeplinkListenerInitialized", "");
+      }, 400);
+    });
+  }
+
 }
+const myCapacitorApp = new ElectronCapacitorApp();
+if (CapacitorFileConfig.deepLinkingEnabled) {
+  setupElectronDeepLinking(myCapacitorApp, DeepLinkingConfig);
+}
+////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-app.on('open-file', (event, path) => {
-  // Support for Mac OS
-  openFiles=[fs.readFileSync(path, null)];
-  const mainWindow = myCapacitorApp.getMainWindow();
-  mainWindow.webContents.send('fileopen', openFiles)
-})
+// Run Application
+(async () => {
+  await app.whenReady();
+  await myCapacitorApp.init()
+})();
 
-app.on('web-contents-created', (createEvent, contents) => {
-  contents.setWindowOpenHandler(({ url }) => {
-    shell.openExternal(url);
-    return { action: 'deny' }
-  })
-});
-
-// Quit when all windows are closed.
 app.on("window-all-closed", function () {
   // On OS X it is common for applications and their menu bar
   // to stay active until the user quits explicitly with Cmd + Q
@@ -93,19 +146,12 @@ app.on("window-all-closed", function () {
   }
 });
 
-app.on("activate", function () {
+app.on("activate", async function () {
   // On OS X it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
-  if (myCapacitorApp.getMainWindow().isDestroyed()) myCapacitorApp.init();
+  if (myCapacitorApp.getMainWindow().isDestroyed()) { 
+    await myCapacitorApp.init();
+  }
 });
 
-// Define any IPC or other custom functionality below here
-function getTorrents (args: Array<any>): Array<Buffer> {
-  const result=[];
-  args.forEach((arg: string) => {
-    if(arg && arg.toLowerCase().endsWith(".torrent")){
-      result.push(fs.readFileSync(arg, null));
-    }
-  });
-  return result;
-}
+// Place all ipc or other electron api calls and custom functionality under this line
