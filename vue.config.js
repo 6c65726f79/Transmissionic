cadd717afc00cea20784b194e313cdb8aefa4f88
Samuel <73540958+6c65726f79@users.noreply.github.com>
2021-07-03 17:55:28 +0200

    Fix @vue/reactivity

--- vue.config.js
+++ vue.config.js
@@ -3,6 +3,7 @@ const webpack = require('webpack')
 const packageJson = fs.readFileSync('./package.json')
 const version = JSON.parse(packageJson).version || 0
 module.exports = {
+    transpileDependencies: ['@vue/reactivity'],
     productionSourceMap: false,
     configureWebpack: {
         plugins: [
