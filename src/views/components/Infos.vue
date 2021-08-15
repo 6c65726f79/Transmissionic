dbd1982701c8a962c4bb233aecb0592b91ce1bea
Samuel <73540958+6c65726f79@users.noreply.github.com>
2021-08-15 16:47:28 +0200

    Update torrents status

--- src/views/components/Infos.vue
+++ src/views/components/Infos.vue
@@ -177,7 +177,7 @@ export default defineComponent({
       details:{} as Record<string,any>,
       statusText:[
         Locale.filters.stopped,
-        "Waiting",
+        Locale.filters.verifying // Waiting for verification,
         Locale.filters.verifying,
         Locale.filters.queued,
         Locale.filters.downloading,
