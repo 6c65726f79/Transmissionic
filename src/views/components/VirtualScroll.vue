03f750a662b06ad38d0920776da705e9a540b67d
Samuel <73540958+6c65726f79@users.noreply.github.com>
2021-11-03 19:45:02 +0100

    Use VueVirtualScroller

--- src/views/components/VirtualScroll.vue
+++ src/views/components/VirtualScroll.vue
@@ -1,16 +1,28 @@
 <template>
-  <ion-content :fullscreen="fullscreen" ref="virtualscroll" :scroll-events="true" @ionScroll="handleScroll($event)">
-    <slot name="start"></slot>
-    <div class="wrapper" ref="wrapper" v-if="virtualList.length>0">
-      <div class="visiblePart" ref="visiblePart">
-        <slot v-for="item in virtualList" :key="item[keyField]" :item="item"></slot>
-      </div>
-    </div>
-    <div class="placeholder" v-else>
-      <strong class="capitalize">{{ Locale.empty }}</strong>
-      <p><slot name="search"></slot></p>
-    </div>
+  <ion-content :fullscreen="fullscreen" ref="virtualscroll">
     <slot name="fab"></slot>
+
+    <RecycleScroller
+      class="scroller"
+      ref="scroller"
+      :items="items"
+      :item-size="itemSize"
+      :key-field="keyField"
+    >
+      <template v-slot:before>
+        <slot name="start"></slot>
+
+        <div class="placeholder" v-if="items.length==0">
+          <strong class="capitalize">{{ Locale.empty }}</strong>
+          <p><slot name="search"></slot></p>
+        </div>
+      </template>
+
+      <template v-slot:default="{item}">
+        <slot :item="item"></slot>
+      </template>
+    </RecycleScroller>
+    
   </ion-content>
 </template>
 
@@ -22,6 +34,8 @@ import {
 import { Locale } from "../../services/Locale";
 import { Utils } from "../../services/Utils";
 
+import 'vue-virtual-scroller/dist/vue-virtual-scroller.css'
+
 export default defineComponent({
   props: ['items','itemSize',"keyField","fullscreen"],
   data() {
@@ -32,51 +46,26 @@ export default defineComponent({
   components: {
     IonContent,
   },
-  computed: {
-    virtualList: function (): Array<any> {
-      const margin=10;
-      const virtualscroll = this.$refs.virtualscroll as Record<string,any>;
-      const scrollheight = virtualscroll?.$el.offsetHeight || window.innerHeight;
-      let visibleCount = Math.floor(scrollheight/this.itemSize)+margin;
-      visibleCount += (this.scrollIndex>margin/2) ? margin : this.scrollIndex;
-      const start = (this.scrollIndex-margin>=0) ? this.scrollIndex-margin : 0
-      const end = (start+visibleCount<=this.items.length) ? start+visibleCount : this.items.length
-      
-      this.setWrapperHeight()
-
-      const visible = this.$refs.visiblePart as Record<string,any>;
-      if(visible){
-        visible.style.top=start*this.itemSize+"px";
-      }
-      return this.items.slice(start, end);
-    }
-  },
   setup() {
     return {
       Locale
     }
   },
   mounted() {
-    // Custom scrollbar for Web Browser and Electron
-    Utils.customScrollbar(this.$refs.virtualscroll);
-    this.setWrapperHeight();
+    const scroller = this.$refs.scroller as any;
+
+    Utils.customScrollbar(scroller, true, false);
+
+    scroller.$el.onwheel = this.handleScroll;
   },
   methods: {
-    handleScroll (event: any) {
-      this.scrollIndex=Math.floor(event.detail.scrollTop/this.itemSize);
-      
+    handleScroll (e: any) {
       const container = this.$refs.virtualscroll as Record<string,any>;
-      if(event.detail.velocityY>0){
+      if(e.deltaY>0){
         container.$el.classList.add("no-fab");
       }
       else {
         container.$el.classList.remove("no-fab");
-      }
-    },
-    setWrapperHeight() {
-      const inner = this.$refs.wrapper as Record<string,any>;
-      if(inner){
-        inner.style.height=this.items.length*this.itemSize+"px";
       }
     }
   }
@@ -84,15 +73,8 @@ export default defineComponent({
 </script>
 
 <style scoped>
-.wrapper {
-  width:100%;
-  position: relative;
-}
-
-.visiblePart {
-  position:absolute;
-  width:100%;
-  padding-bottom: var(--offset-bottom); /* Fix for Firefox */
+.scroller {
+  height:100%;
 }
 
 .placeholder {
