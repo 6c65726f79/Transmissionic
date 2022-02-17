30ddd4126f583795411f6838ececd518f948694b
Samuel <73540958+6c65726f79@users.noreply.github.com>
2022-02-17 23:10:00 +0100

    Update dependencies

--- vue.config.js
+++ vue.config.js
@@ -2,16 +2,16 @@ const fs = require('fs')
 const webpack = require('webpack')
 const packageJson = fs.readFileSync('./package.json')
 const version = JSON.parse(packageJson).version || 0
+const NodePolyfillPlugin = require("node-polyfill-webpack-plugin")
 module.exports = {
     transpileDependencies: ['@vue/reactivity'],
     productionSourceMap: false,
     configureWebpack: {
         plugins: [
             new webpack.DefinePlugin({
-                'process.env': {
-                    PACKAGE_VERSION: '"' + version + '"'
-                }
-            })
+                'process.env.PACKAGE_VERSION': '"' + version + '"'
+            }),
+            new NodePolyfillPlugin()
         ]
     }
 }
