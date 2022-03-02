5f859d79ea70bb1da61416c33af31280c6d3cf99
Samuel <73540958+6c65726f79@users.noreply.github.com>
2022-03-02 21:17:31 +0100

    Allows to open explorer from torrent list (#781)

--- src/views/TorrentDetails.vue
+++ src/views/TorrentDetails.vue
@@ -376,7 +376,7 @@ export default defineComponent({
         },
         {
           text: Locale.actions.openInExplorer,
-          handler: () => this.openExplorer()
+          handler: () => FileHandler.openExplorer(this.privateState.details)
         },
         {
           text: Locale.actions.copyMagnet,
@@ -466,18 +466,6 @@ export default defineComponent({
           ],
         });
       return alert.present();
-    },
-    openExplorer() {
-      let isFile = false;
-      let path = this.privateState.details.name;
-      const directory = this.privateState.details.downloadDir;
-
-      if(this.privateState.details.files.length===1){
-        path = this.privateState.details.files[0].name
-        isFile = true;
-      }
-
-      FileHandler.openExplorer(directory,path,isFile)
     },
     async setLocation() {
       const modal = await modalController
